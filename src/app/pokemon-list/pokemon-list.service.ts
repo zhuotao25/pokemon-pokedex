eb96f40f4197b6fa0a81535057e7acad9b0c460a
michael.zt <7454681+zhuotao25@users.noreply.github.com>
2019-09-01 00:31:49 -0400

    Load pokemon list from json

--- src/app/pokemon-list/pokemon-list.service.ts
+++ src/app/pokemon-list/pokemon-list.service.ts
@@ -1,36 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonListService {
 
-  private pokemons: Pokemon[] = [
-    {
-      id: '1',
-      name: 'Bulbasaur',
-      imageUrl:'https://github.com/fanzeyi/pokemon.json/blob/master/images/001.png?raw=true',
-      types: ['Grass', 'Poison']
-    },
-    {
-      id: '2',
-      name: 'Ivysaur',
-      imageUrl:'https://github.com/fanzeyi/pokemon.json/blob/master/images/002.png?raw=true',
-      types: ['Grass', 'Poison']
-    },
-    {
-      id: '3',
-      name: 'Venusaur',
-      imageUrl:'https://github.com/fanzeyi/pokemon.json/blob/master/images/003.png?raw=true',
-      types: ['Grass', 'Poison']
-    }
-  ]
+  private pokemons: Pokemon[] = []
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getAllPokemons(){
-    return [...this.pokemons];
+    let url = 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json';
+    let data: Observable<any> = this.http.get(url);
+    data.subscribe( results =>{
+      for( var i in results ){
+        var result = results[i];
+        let pokemon: Pokemon = <Pokemon>{};
+        pokemon.id = String(result["id"]);
+        pokemon.name = String(result["name"]["english"]);
+        pokemon.types = result["type"];
+        if(pokemon.id.length==1)pokemon.id='00'+pokemon.id;
+        else if(pokemon.id.length==2)pokemon.id='0'+pokemon.id;
+        pokemon.imageUrl = 'https://github.com/fanzeyi/pokemon.json/blob/master/images/'+pokemon.id+'.png?raw=true'
+       
+        this.pokemons.push(pokemon);
+      }
+    });
+    
+    return this.pokemons;
   }
 
   getPokemon(pokemonId: string){
